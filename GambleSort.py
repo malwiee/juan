331ed5253b1a8a6997a9de31d031896a80554546
Emillime04 <simonettiemil@gmail.com>
2025-01-14 10:16:37 +0100

    Let's go gambling!

---
+++ GambleSort.py
@@ -0,0 +1,13 @@
+import random
+
+x = int(input("Inserisci un numero: "))
+y = random.randint(0, 1000000)
+print("Let's go gambling!")
+
+while x != y:
+    y = random.randrange(0, 1000000)
+    print("Ah dangit!")
+    print("Let's go gambling!")
+
+
+print("I found your number: ")
